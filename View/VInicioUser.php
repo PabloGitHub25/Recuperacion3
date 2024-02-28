4c220280d8eb22a97779e206b868ad80420f3d52
PabloGitHub25 <ldupablo21@gmail.com>
2024-02-28 17:36:23 -0500

    .

--- View/VInicioUser.php
+++ View/VInicioUser.php
@@ -3,13 +3,16 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../Recuperacion3/css/styleUser.css">
+    <link rel="stylesheet" href="../css/styleUser.css">
     <title>PAGINA INICIAL</title>
 </head>
 <body>
+    <br><br><br>
+    <br><br><br>
+    <br><br><br><br>
     <div class="container">
         <h1>Bienvenido a la Página Inicial</h1>
-        <img src="../path-to-your-image.jpg" alt="Imagen">
+        <img src="../public/img/inicio.png" alt="Imagen">
         <div class="options">
             <a href="ingresar_tareas.php" class="button">Ingresar tareas nuevas</a>
             <a href="editar_tareas.php" class="button">Editar tareas</a>
