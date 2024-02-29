f9fc5e695a80b0960b54e4b34e4d398dd86d5e06
PabloGitHub25 <ldupablo21@gmail.com>
2024-02-28 21:47:39 -0500

    .

--- Model/MIngresoTareaU.php
+++ Model/MIngresoTareaU.php
@@ -9,11 +9,12 @@ if (isset($_SESSION['idUser'])) {
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
         // Verificar si se han seleccionado tareas
         if (isset($_POST["tareas"])) {
-            foreach ($_POST["tareas"] as $nombreTarea) {
-                // Insertar la tarea en la tabla usuario_tareas
-                $sqlInsert = "INSERT INTO usuario_tarea (idUser, idTarea, estado) VALUES ('$idUser', (SELECT idTarea FROM tareas WHERE nombreTarea='$nombreTarea'), 'Pendiente')";
-                mysqli_query($conexion, $sqlInsert);
-            }
+            $tareasSeleccionadas = implode("','", $_POST["tareas"]);
+            
+            // Insertar las tareas en la tabla usuario_tareas
+            $sqlInsert = "INSERT INTO usuario_tarea (idUser, idTarea, estado) SELECT '$idUser', idTarea, 'Pendiente' FROM tareas WHERE nombreTarea IN ('$tareasSeleccionadas')";
+            mysqli_query($conexion, $sqlInsert);
+            
             echo "Tareas agregadas correctamente";
         } else {
             echo "No se han seleccionado tareas";
