1e340ebd5cc305f68914995051019955dc2e2754
PabloGitHub25 <ldupablo21@gmail.com>
2024-03-01 15:24:55 -0500

    .

--- View/VRegistro.php
+++ View/VRegistro.php
@@ -3,8 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../Recuperacion3/css/style.css">
     <title> REGISTRO</title>
+    <link rel="stylesheet" href="../css/style.css">
 </head>
 <body>
     <div class="modal">
