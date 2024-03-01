6d93cdeeb9d2bbb3293f77f991136101319dce89
PabloGitHub25 <ldupablo21@gmail.com>
2024-03-01 12:10:50 -0500

    .

---
+++ View/VInicioAdmin.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/styleUser.css">
+    <title>PAGINA INICIAL</title>
+</head>
+<body>
+    <br><br><br>
+    <br><br><br>
+    <br><br><br><br>
+    <div class="container">
+        <h1>Bienvenido a la Página Inicial</h1>
+        <img src="../public/img/inicio.png" alt="Imagen">
+        <div class="options">
+            <a href="../Controller/contolador.php?var1=1" class="button">Agregar tareas nuevas</a>
+            <a href="editar_tareas.php" class="button">Editar tareas</a>
+            <a href="editar_tareas.php" class="button">Eliminar tareas</a>
+            <a href="informe_tareas.php" class="button">Informes</a>
+        </div>
+    </div>
+</body>
+</html>
