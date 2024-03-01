47a1bc1d51374f04e49fee2d7fa5b94464c2ca9a
Erick Cajamarca <erick_cajamarca@hotmail.com>
2024-03-01 15:54:53 -0500

    .

---
+++ View/VRegistroExitoso.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Registro Exitoso</title>
+    <link rel="stylesheet" href="../css/style.css">
+</head>
+<body>
+    <div class="modal">
+        <div class="modal-content">
+            <h1>Registro Exitoso</h1>
+            <p>Tu registro ha sido exitoso. Ahora puedes iniciar sesión con tu cuenta.</p>
+            <button onclick="window.location.href='../index.html'" class="button">Iniciar Sesión</button>
+        </div>
+    </div>
+</body>
+</html>
